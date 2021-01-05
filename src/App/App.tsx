4d93b4669560915fceca73e64872cf2c6555531c
JP Carey <jpcarey4@gmail.com>
2021-01-05 16:34:17 -0700

    add components files, scss variables, park data array

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss'
+import React, { useState, useEffect } from 'react'
+
+
+function App() {
+  return (
+    <h1>hello... its me</h1>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App.tsx';
+import App from './App/App.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
