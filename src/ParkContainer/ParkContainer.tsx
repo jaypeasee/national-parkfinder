40accd7fde2998fd022df415c70b69abdc392c9c
Sarah Osgood <sarahjs117@gmail.com>
2021-01-06 16:30:19 -0700

    Modifiying to use interface file but all is not working correctly yet

--- src/ParkContainer/ParkContainer.tsx
+++ src/ParkContainer/ParkContainer.tsx
@@ -3,39 +3,23 @@ import Banner from '../Banner/Banner'
 import UserNav from '../UserNav/UserNav'
 import ParkInfo from '../ParkInfo/ParkInfo'
 import React, { useState, useEffect } from 'react'
+import { ParkCode, Activities, Contacts, Images, CurrentPark } from './interfaces'
 
-interface ParkCode {
-  parkCode: string
-}
+type Props = ParkCode | Activities | Contacts | Images | CurrentPark
 
-interface Response {
-  response: Array<object>
-}
-
-// interface Booking {
-//   id: string
-//   userID: number
-//   date: string
-//   roomNumber: string
-// }
-
-interface ParkData {
-  name: string
-  state: string
-  parkCode: string
-  directionsInfo: string
-}
-
-const ParkContainer: React.FC<ParkCode> = props => {
-  const [currentPark, setCurrentPark] = useState([])
+const ParkContainer: React.FC<Props> = props => {
+  const [currentPark, setCurrentPark] = useState([] || '')
+  // const [error, setError] = useState('')
   const { parkCode } = props as ParkCode
-
   const request = async () => {
     const response = await fetch(
-      `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=2hfS4AxtbC4JiDwsIX0nEC4iVLXP57q8sUoEHUXp`,
-    );
-    const data = await response.json();
-    setCurrentPark(data.data);
+      // `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`,
+      `https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`
+    )
+    const data = await response.json()
+    .catch(error => setCurrentPark(error.message))
+    setCurrentPark(data.bookings)
+    // setCurrentPark(data.data)
     return data
   }
 
@@ -51,25 +35,5 @@ const ParkContainer: React.FC<ParkCode> = props => {
     </section>
   )
 }
-
-// const ParkContainer: React.FC<ParkCode> = props => {
-//   const [currentPark, setCurrentPark] = useState({})
-//   const { parkCode } = props as ParkCode
-  
-//   useEffect(() => {
-//     return fetch(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=2hfS4AxtbC4JiDwsIX0nEC4iVLXP57q8sUoEHUXp`)
-//     // .then(response => response.json())
-//     .then(response => setCurrentPark(response))
-//     .catch(error => console.log(error))
-//   }, {})
-
-//   return (
-//     <section>
-//         <Banner />
-//         <UserNav />
-//         <ParkInfo />
-//     </section>
-//   )
-// }
 
 export default ParkContainer;
