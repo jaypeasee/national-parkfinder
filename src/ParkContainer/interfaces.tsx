6675f2d1613713bde5814c136a131ec41f37d21e
Sarah Osgood <sarahjs117@gmail.com>
2021-01-06 16:29:02 -0700

    Add interfaces file for ParkContainer

---
+++ src/ParkContainer/interfaces.tsx
@@ -0,0 +1,40 @@
+export interface ParkCode {
+  parkCode: string
+}
+
+export interface Activities {
+  id: string
+  name: string
+}
+
+export interface Contacts {
+  phoneNumber: string
+  description: string
+  extension: string
+  type: string
+}
+
+export interface Images {
+  credit: string
+  title: string
+  altText: string
+  caption: string
+  url: string
+}
+
+export interface CurrentPark {
+  name: string
+  fullName: string
+  description: string
+  directions: string
+  latitude: string
+  longitude: string
+  activities: Activities
+  contacts: Contacts
+  directionsInfo: string
+  directionsUrl: string
+  weatherInfo: string
+  image: Images
+  id: string
+  url: string
+}
