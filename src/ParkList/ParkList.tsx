b031ce323a9da5239ab2f9ef589eac718f2af913
Sarah Osgood <sarahjs117@gmail.com>
2021-01-06 10:08:51 -0700

    Add interface and typescript sytax to map through national park data objects

--- src/ParkList/ParkList.tsx
+++ src/ParkList/ParkList.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { nationalParks } from '../ParkData'
 import './ParkList.scss'
 import ParkBtn from '../ParkBtn/ParkBtn'
+import { JsxElement } from 'typescript'
 
-function ParkList() {
-    return (
-        <ParkBtn />
-    )
+// interface ParkList {
+//   children: JSX.Element
+// }
+
+interface Park { 
+  name: string
+  parkCode: string
+  image: string
+  state: string
+}
+
+function ParkList(): JSX.Element {
+  const parksOnDisplay = nationalParks.map(park => {
+    console.log(park)
+    return <ParkBtn
+      name={park.name}
+      parkCode={park.parkCode}
+      image={park.image}
+      state={park.state}
+    />
+  })
+  return (
+    <section>
+      {parksOnDisplay}
+    </section>
+  )
 }
 
 export default ParkList
