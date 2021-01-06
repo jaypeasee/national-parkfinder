48ad7c089250794d309310cb4159c33dd61abcc1
Sarah Osgood <sarahjs117@gmail.com>
2021-01-06 13:28:30 -0700

    set up index to use Router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import './index.scss'
 import App from './App/App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
+const router = <BrowserRouter> <App /></BrowserRouter>
 ReactDOM.render(
-  <App />,
+  router,
   document.getElementById('root')
 );
