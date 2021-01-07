69c05f5057fc107c75c049b3fa4dccfe0ecd9541
JP Carey <jpcarey4@gmail.com>
2021-01-06 18:18:27 -0700

    fix useEffect to cut down number of calls, move api call to separate file

--- src/ParkContainer/ParkContainer.tsx
+++ src/ParkContainer/ParkContainer.tsx
@@ -4,28 +4,19 @@ import UserNav from '../UserNav/UserNav'
 import ParkInfo from '../ParkInfo/ParkInfo'
 import React, { useState, useEffect } from 'react'
 import { ParkCode, Activities, Contacts, Images, CurrentPark } from './interfaces'
+import { parkRequest } from './npsApiCall'
 
 type Props = ParkCode | Activities | Contacts | Images | CurrentPark
 
 const ParkContainer: React.FC<Props> = props => {
   const [currentPark, setCurrentPark] = useState([] || '')
-  // const [error, setError] = useState('')
   const { parkCode } = props as ParkCode
-  const request = async () => {
-    const response = await fetch(
-      // `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=C7eGnlc0BVmtjubOSmuD8P6UPmcRfLM4ewoeNBuI`,
-      `https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`
-    )
-    const data = await response.json()
-    .catch(error => setCurrentPark(error.message))
-    setCurrentPark(data.bookings)
-    // setCurrentPark(data.data)
-    return data
-  }
 
   useEffect(() => {
-    request()
-  })
+    parkRequest(parkCode)
+      .then(data => setCurrentPark(data.data))  
+      .catch(error => setCurrentPark(error.message))
+  }, [parkCode])
 
   return (
     <section>
