050fd571c0479a3e9052b42545d851eab6066f64
JP Carey <jpcarey4@gmail.com>
2021-01-07 21:04:35 -0700

    set state on the name search of the nav form

--- src/NavSearch/NavSearch.tsx
+++ src/NavSearch/NavSearch.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom'
 import './NavSearch.scss'
 
-function NavSearch() {
+const NavSearch: React.FC = () => {
+    const [nameSearch, setNameSearch] = useState<string>('')
+
     return(
-        <form>
+        <form
+            className='nav-search'
+        >
             <input
-                placeholder="search by name"
+                placeholder='search by name'
+                value={nameSearch}
+                onChange={event => setNameSearch(event.target.value)}
+                name="name"
             />
             <input 
-                placeholder="state"
+                placeholder='state'
             />
         </form>
     )
