aea57ca12ce253b3792b4427cc748e38a68288b2
JP Carey <jpcarey4@gmail.com>
2021-01-07 22:06:56 -0700

    push up filter error for others to try fixing

--- src/MainNav/MainNav.tsx
+++ src/MainNav/MainNav.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './MainNav.scss';
 import NavSearch from '../NavSearch/NavSearch'
 import ParkList from '../ParkList/ParkList'
@@ -6,24 +7,42 @@ import { nationalParks } from '../ParkData'
 import ParkBtn from '../ParkBtn/ParkBtn'
 import { LocalParkData } from '../interfaces'
 
-
 interface ChoosePark {
   choosePark: (parkCode: string) => void
 }
 
-type NavProps = ChoosePark | LocalParkData
+interface FilterButtonsByName {
+    filterButtonsByName: (searchTerm: string) => any
+}
+
+type NavProps = ChoosePark | LocalParkData | FilterButtonsByName
 
 const MainNav: React.FC<NavProps> = (props) => {
     const { choosePark } = props as ChoosePark
+    const [nameSearch, setNameSearch] = useState<string>('')
+    const [parksOnDisplay, setParksOnDisplay] = useState<Array<LocalParkData>>()
 
-    const parksOnDisplay = nationalParks.map(park => {
-    return <ParkBtn
-      key={park.parkCode}
-      name={park.name}
-      parkCode={park.parkCode}
-      choosePark={choosePark}
-    />
-  })
+    useEffect(() => {
+        const parkButtons: any = nationalParks.map(park => {
+            return <ParkBtn
+                key={park.parkCode}
+                name={park.name}
+                parkCode={park.parkCode}
+                choosePark={choosePark}
+            />
+        })
+        setParksOnDisplay(parkButtons)
+    }, [nameSearch])
+
+    const filterButtonsByName = (searchTerm: string): FilterButtonsByName => {
+        if (parksOnDisplay) {
+            setNameSearch(searchTerm)
+            const filteredParks = parksOnDisplay.filter(park => {
+            return park.name.includes(searchTerm)
+        })
+            setParksOnDisplay(filteredParks)
+        }
+    }
 
     return (
         <nav className="main-nav">
@@ -33,7 +52,9 @@ const MainNav: React.FC<NavProps> = (props) => {
                 alt="National Parkfinder Logo"
             />
             <h1>National Parkfinder</h1>
-            <NavSearch />
+            <NavSearch 
+                filterButtonsByName={filterButtonsByName}
+            />
             <ParkList 
                 choosePark={choosePark}
                 parksOnDisplay={parksOnDisplay} 
