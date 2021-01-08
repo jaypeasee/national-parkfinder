ce56b0e8a60077c98416b45d8b1763763695f960
Bailey Dunning <bwdunning@gmail.com>
2021-01-08 14:28:13 -0700

    Fix location bug

--- src/Location/Location.tsx
+++ src/Location/Location.tsx
@@ -3,17 +3,14 @@ import './Location.scss'
 
 const Location: React.FC<CurrentParkContainer> = props => {
   const { currentPark } = props as CurrentParkContainer 
-  console.log(currentPark)
-  console.log('helllo')
     return (
       <section>
-        <p><b>Driving directions to {currentPark.name}: </b>{currentPark.directions}</p>
+        <p><b>Driving directions to {currentPark.name}: </b>{currentPark.directionsInfo}</p>
         <iframe 
             src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d196281.64169563106!2d${currentPark.longitude}!3d${currentPark.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1610139788967!5m2!1sen!2sus`} 
-            width = "600" height = "450">
+            width = "800" height = "200">
         </iframe >
       </section>
-      
     )
 }
 
