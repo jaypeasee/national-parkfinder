139b4bb1e68ee36303d401ac734753cc20d43b5f
Sarah Osgood <sarahjs117@gmail.com>
2021-01-08 16:55:03 -0700

    Complete functionality for displaying park info

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -4,20 +4,21 @@ import './Contact.scss'
 
 const Contact: React.FC<CurrentParkContainer> = props => {
   console.log(props)
-  const { currentPark } = props as CurrentParkContainer 
-  const { contacts, emailAddresses} = currentPark as any 
+  const { currentPark } = props as CurrentParkContainer
+  const { contacts, emailAddresses } = currentPark as any
   const { addresses } = currentPark as any
 
   const parsedPhoneNumber: any = parsePhoneNumber(`+1${currentPark.contacts.phoneNumbers[0].phoneNumber}`)
   console.log(parsePhoneNumber)
 
-    return (
-      <section>
-        <p><b>Email: </b>{currentPark.contacts.emailAddresses[0].emailAddress}</p>
-        <p><b>PhoneNumber: </b>{parsedPhoneNumber.format("NATIONAL")}</p>
-        <p><b>Mailing Address: </b>{addresses[0].city}</p>
-      </section>
-    )
+  return (
+    <section>
+      <p><b>Email: </b>{currentPark.contacts.emailAddresses[0].emailAddress}</p>
+      <p><b>PhoneNumber: </b>{parsedPhoneNumber.format("NATIONAL")}</p>
+      <p><b>Mailing Address: </b></p>
+      <p>{addresses[0].city}</p>
+    </section>
+  )
 }
 
 export default Contact
