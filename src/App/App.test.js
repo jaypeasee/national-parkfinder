2c308d764df60f40c9428bd279f317c14e19a8e3
Sarah Osgood <sarahjs117@gmail.com>
2021-01-10 19:23:21 -0700

    Add async test for displaying a park in the container

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -4,18 +4,41 @@ import '@testing-library/jest-dom'
 import { samplePark } from '../samplePark'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
+import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
-  const history = createMemoryHistory()
-
+  
   beforeEach(() => {
+    const history = createMemoryHistory()
     render(
       <Router history={history}>
         <App />
       </Router>
     )
   })
-  it('should render the home page of the app', () => {
 
+  it('should render a park when user clicks a park button', async () => {
+    const acadiaButton = screen.getByText('Acadia')
+    userEvent.click(acadiaButton)
+    const acadiaPark = await waitFor(() => screen.getByText('Acadia National Park, ME'))
+    expect(acadiaPark).toBeInTheDocument()
   })
+
+  // it('should randomly generate a park code on load', async () => {
+  //   // generateRandomParkCode.mockResolvedValueOnce('acad')
+  //   // generateRandomParkCode = jest.fn().mockImplementationOnce(() => 'acad')
+  //   const history = createMemoryHistory()
+  //   history.location.pathname='/acad/about'
+  //   render(
+  //     <Router history={history}>
+  //       <App />
+  //     </Router>
+  //   )
+  //   screen.debug()
+  //   const acadiaPark = await waitFor(() => screen.getByText('Acadia National Park, ME'))
+  //   expect(acadiaPark).toBeInTheDocument()
+  // })
+
+// can we add adn remove from bucket list and visited
+//
 })
