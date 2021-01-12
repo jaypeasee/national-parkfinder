e5001a9f007e55e79b6f911da55a0c0e962ff12c
Bailey Dunning <bwdunning@gmail.com>
2021-01-12 11:19:44 -0700

    Beef up MainNav test with state dropdown

--- src/MainNav/MainNav.test.js
+++ src/MainNav/MainNav.test.js
@@ -24,17 +24,44 @@ describe('MainNav', () => {
   })
 
   it('should render a list of park buttons', () => {
-    expect(screen.getByText('Grand Canyon')).toBeInTheDocument()
     const allButtons = screen.getAllByRole("button")
+    
+    expect(screen.getByText('Grand Canyon')).toBeInTheDocument()
     expect(allButtons.length).toBe(63)
   })
 
   it('should be able to filter the list of park buttons based on name match', () => {
     const searchInput = screen.getByPlaceholderText('search by name')
+    
     userEvent.type(searchInput, 'grand')
     const allButtons = screen.getAllByRole("button")
+    
     expect(allButtons.length).toBe(3)
     expect(screen.getByText('Grand Canyon')).toBeInTheDocument()
+  })
+
+  it('should filter park buttons based off state selection', () => {
+    const stateDropdown = screen.getByTestId('select-form')
+    const minnesota = screen.getByText('Minnesota')
+    
+    userEvent.selectOptions(stateDropdown, [minnesota])
+    const allButtons = screen.getAllByRole("button")
+    
+    expect(allButtons.length).toBe(2)
+    expect(screen.getByText('Voyageurs')).toBeInTheDocument()
+  })
+
+  it('should filter park buttons by state and name search', () => {
+    const stateDropdown = screen.getByTestId('select-form')
+    const colorado = screen.getByText('Colorado')
+    const searchInput = screen.getByPlaceholderText('search by name')
+    
+    userEvent.selectOptions(stateDropdown, [colorado])
+    userEvent.type(searchInput, 'rock')
+    const allButtons = screen.getAllByRole("button")
+
+    expect(allButtons.length).toBe(2)
+    expect(screen.getByText('Rocky Mountain')).toBeInTheDocument()
   })
 })
 
