5ef0aff0dbacdd7fae6a0131265ec68b9455ba87
Bailey Dunning <bwdunning@gmail.com>
2021-01-11 17:21:07 -0700

    Add dropdown search for state in MainNav

---
+++ src/StateDropdown/StateDropdown.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+
+type StateDropdownProps = { setStateSelect: (stateSelection: string) => void }
+
+const StateDropdown: React.FC<StateDropdownProps> = props => {
+  const [stateSelection, setStateSelection] = useState('')
+  const { setStateSelect } = props as StateDropdownProps
+
+  const handleSelect = (event: any) => {
+    setStateSelect(event.target.value)
+    setStateSelection(event.target.value)
+  }
+
+  return (
+    <form onChange={handleSelect}>
+      <fieldset>
+        <label aria-label="state">State</label>
+        <select
+          id="state"
+          name="state">
+          <option value="---">---</option>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AS">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="VI">Virgin Islands</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </select>
+      </fieldset>
+    </form>
+  )
+}
+
+export default StateDropdown
