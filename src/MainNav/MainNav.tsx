471fc02defb2e4a453af8fff5742c0ab9937b4a7
Bailey Dunning <bwdunning@gmail.com>
2021-01-28 15:52:35 -0700

    add functionality to site logo

--- src/MainNav/MainNav.tsx
+++ src/MainNav/MainNav.tsx
@@ -65,6 +65,7 @@ const MainNav: React.FC<NavProps> = (props) => {
         className='logo'
         src={parkfinderLogo}
         alt='National Parkfinder Logo'
+        onClick={() => generateRandomParkCode()}
       />
       <h1 className='site-header'>National Parkfinder</h1>
       <NavSearch setSearch={setSearch}/>
